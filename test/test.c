d166dec5c83e43b2b9c6d03160d333f44e76d428
Paolo Montesel <paolo.montesel+gh@gmail.com>
2018-07-20 18:42:07 +0200

    Updated test file

--- test/test.c
+++ test/test.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
-void dumb() {
-  puts("ITSAYOU!");
-}
-
 int main() {
-  puts("ITSAME!");
+  char c;
+
+  puts("Show me your character:");
+  c = getchar();
+
+  if (c + 0x5A == 0xAA) {
+    puts("You are a proper Prosecco drinker mate, come join the AA!");
+  } else {
+    puts("Looks like we got a non drinker here, sad");
+  }
+
   return 0;
 }
 

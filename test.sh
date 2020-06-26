837ddfb9b8221624e75f0726ffdfd23fd99326af
thebabush <paolo.montesel+gh@gmail.com>
2020-06-26 23:17:10 +0200

    llvm-9: additional changes

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 
 cd test
 echo "====> Compiling LLVM IR..."
-clang-7 -S -emit-llvm test.c
+clang-9 -S -emit-llvm test.c
 echo "====> Obfuscated LLVM IR:"
-opt-7 -load=../build/dumbo/DumboPass.so --dumbo ./test.ll -S -o test_obfuscated.ll
+opt-9 -load=../build/dumbo/DumboPass.so --dumbo ./test.ll -S -o test_obfuscated.ll
 cat test_obfuscated.ll
 echo "====> Compiling final executable..."
-clang-7 -flto=full -march=native -O3 -Wl,-strip-all -o test test_obfuscated.ll ../helper/modexp_wide.ll
+clang-9 -flto=full -march=native -O3 -Wl,-strip-all -o test test_obfuscated.ll ../helper/modexp_wide.ll
 echo "====> Running final executable..."
 ./test
 
